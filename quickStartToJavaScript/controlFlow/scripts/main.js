55a6ce18f682c547fa7c4def4c9104a70510e87b
Won Maung Thein <wonmaungthein@gmail.com>
2019-01-09 21:44:27 +0000

    Added a new folder for quick start to Javascript

---
+++ quickStartToJavaScript/controlFlow/scripts/main.js
@@ -0,0 +1,19 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+// Control Flow Exercise
+
+var answer = prompt("What is your name");
+
+if(answer === "Won" ){
+  alert(`Hello Won, How are you? `)
+}else {
+  alert (`Hello ${answer}. Welcome to my site`)
+};
+
